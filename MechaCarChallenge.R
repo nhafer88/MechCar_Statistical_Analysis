39253e23cf1aff71a596f2ac52b2532edd40edad
Nick <nhafer88@gmail.com>
2022-01-09 15:52:35 -0800

    updated file.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,4 @@
-# Deliverable 1
+#Deliverable 1#
 
 #3 Load dplyr package
 library(tidyverse)
@@ -14,7 +14,37 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #linear regression model.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_table))
 
-#Deliverable 2
+#Deliverable 2#
 
 #2 Import CSV as table
 Coil_table <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+
+#3 Write an RScript that creates a total_summary dataframe using the summarize() function to get the mean, 
+#median, variance, and standard deviation of the suspension coil's PSI column.
+coil_summary <-Coil_table %>%
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+#4 Group by
+lot_summary <-Coil_table %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+#Deliverable 3#
+
+#1 Use t.test() function to determine if the PSI across all manufacturing lots is statistically different 
+#from the population mean of 1,500 pounds per square inch.
+t.test(Coil_table$PSI, mu=1500)
+
+#2 Using the t.test() function and its subset() argument to determine if the PSI for each manufacturing lot 
+#is statistically different from the population mean of 1,500 pounds per square inch:
+
+#a) PSI for manufacturing-lot1 using subset()
+population1 = subset(Coil_table, Manufacturing_Lot =='Lot1')
+t.test(population1$PSI, mu=1500)
+
+#b) PSI for manufacturing-lot2 using subset()
+population2 = subset(Coil_table, Manufacturing_Lot =='Lot2')
+t.test(population2$PSI, mu=1500)
+
+#c) PSI for manufacturing-lot3 using subset()
+population3 = subset(Coil_table, Manufacturing_Lot =='Lot3')
+t.test(population3$PSI, mu=1500)
